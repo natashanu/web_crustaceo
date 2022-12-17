004bcefe639fbf7e9890fe74b9b4b86de5d08f00
Iago Pereiro <pereiro600@gmail.com>
2022-12-17 18:22:10 +0100

    Descripcion

--- js/basedeDatos.js
+++ js/basedeDatos.js
@@ -7,21 +7,22 @@ var complementos = new Array();
 
 
 //Nombre Alimento, Nombre imagen, Descripcion, Alergenos, Precio
-complementos.push(['Algas Fritas', 'algasFritas.jpg', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione eius animi ipsam, dignissimos sed beatae rem possimus at sit odit quasi veniam. Mollitia excepturi culpa eius necessitatibus eos! Saepe, reprehenderit!','',5.0]);
-complementos.push(['Perro Caliente', 'perroCaliente.jpg', 5.0]);
-complementos.push(['Pizza de Don Cangrejo', 'Pizza.jpg', 5.0]);
-complementos.push(['Bracitos Crujientes', 'maniManitas.jpg', 5.0]);
-bebidas.push(['Refresco de Algas', 'bebidas.jpg', 2.0]);
-bebidas.push(['Malteada', 'Merengada.png', 5.0]);
-menuInfantil.push(['Caja Sorpresa', 'CajitaFeliz.jpg', 5.0]);
-menuInfantil.push(['Menú Infantil', 'menuInfantil.jpg', 5.0]);
-menuInfantil.push(['Mini Cangreburguer', 'MiniCangre.jpg', 5.0]);
-hamburguesas.push(['Cangreburguer Sin Queso', 'cangreNoQueso.jpg','orem ipsum dolor sit amet consectetur adipisicing elit. Ratione eius animi ipsam, dignissimos sed beatae rem possimus at sit odit quasi veniam. Mollitia excepturi culpa eius necessitatibus eos! Saepe, reprehenderit!', '', 5.0]);
-hamburguesas.push(['Cangreburguer Con Queso', 'CangreQueso.jpg', 5.0]);
-hamburguesas.push(['Hamburguesa Naranja', 'hamburguesasNar.jpg', 5.0]);
-hamburguesas.push(['Hamburguesa Amarilla', 'hamburguesasAmarillo.jpg', 5.0]);
-hamburguesas.push(['Hamburguesa Azul', 'hamburguesasAzul.jpg', 5.0]);
-hamburguesas.push(['Hamburguesa Morada', 'hamburguesasMorada.jpg', 5.0]);
-hamburguesas.push(['Hamburguesa Roja', 'hamburguesasRoja.jpg', 5.0]);
-hamburguesas.push(['Hamburguesa Verde', 'hamburguesasVerde.jpg', 5.0]);
+complementos.push(['Algas Fritas', 'algasFritas.jpg', 'Las famosas algas fritas de las que tanto has oído hablar, si estás son, las mejores, las más crujientes, las que tienen más sabor, si las pruebas entenderás el por qué de su fama. Disponibles en varios tamaños.',5.0]);
+complementos.push(['Perro Caliente', 'perroCaliente.jpg','Jugoso perrito caliente preparado con salchicha sola envuelta en un jugoso pan con ketchup, mostaza o mañonesa.', 5.0]);
+complementos.push(['Pizza de Don Cangrejo', 'Pizza.jpg','Nuestra Pizza más dulce con alga tostada, jugoso pollo marinado y carne de vacuno, acompañada de nuestra adictiva salsa .', 5.0]);
+complementos.push(['Bracitos Crujientes', 'maniManitas.jpg','¿No querías Bracitos? ¡Pues toma un cubo entero! Te traemos 24 bocados del mejor pollo, perfecto para compartir y mojar en nuestras deliciosas salsas. ¡Pídelo y moja!', 5.0]);
+bebidas.push(['Refresco', 'Refresco.png','Refrescante bebida para acompañar a tu pedido', 5.0]);
+bebidas.push(['Refresco de Algas', 'bebidas.jpg','Que no te sorprenda su color, extraido de las mejores cosechas del granjero Bob. Recipiente 100% organico', 2.0]);
+bebidas.push(['Malteada', 'Merengada.png','¡Pasa de verlo, a comerlo! Deliciosa Malteada con nuestro riquísimo helado más trozos de coral marino. Disfrútalo con o sin nata, como más te guste.', 5.0]);
+menuInfantil.push(['Caja Sorpresa', 'CajitaFeliz.jpg','Pide tu caja con grandes sorpresas en tu interior. Te sorprenderan las figuras de tus heroes favoritos.', 5.0]);
+menuInfantil.push(['Menú Infantil', 'menuInfantil.jpg','La menu infantil te trae la fórmula perfecta. Un menú completo y diversión sin fin con la sorpresa que esconde en su interior. La mejor opción para cubrir las necesidades tanto de padres como de los más pequeños.', 5.0]);
+menuInfantil.push(['Mini Cangreburguer', 'MiniCangre.jpg','Para esos pececitos que no tienen mucha hambre y quieren disfrutar de la deliciosa mini Cangreburger personalizada', 5.0]);
+hamburguesas.push(['Cangreburguer Sin Queso', 'cangreNoQueso.jpg','Nuestra carne a parrilla acompañada de tomate, lechuga, kétchup y mostaza. Lo bueno y sencillo, dos veces bueno.', '', 5.0]);
+hamburguesas.push(['Cangreburguer Con Queso', 'CangreQueso.jpg','El Cangreburguer siempre será nuestro número uno. Jugosa carne doble de vacuno de estupenda calidad a la parrilla, tomate y lechuga fresca traídos de la huerta de Patricio, suave cebolla y sabroso pepinillo acompañado de mayonesa y kétchup. No olvides el esponjoso pan de semillas, que hacen en su conjunto una hamburguesa de sabor único y que reconocerías con los ojos cerrados.', 5.0]);
+hamburguesas.push(['Hamburguesa Naranja', 'hamburguesasNar.jpg','¿Una Burger vegetariana con sabor especial? Nueva Vegetal de color naranja. 100% sabor a zanahoria', 5.0]);
+hamburguesas.push(['Hamburguesa Amarilla', 'hamburguesasAmarillo.jpg','¿Una Burger vegetariana con sabor especial? Nueva Vegetal de color amarilla. 100% sabor a piña', 5.0]);
+hamburguesas.push(['Hamburguesa Azul', 'hamburguesasAzul.jpg','¿Una Burger vegetariana con sabor especial? Nueva Vegetal de color azul. 100% sabor a arandanos', 5.0]);
+hamburguesas.push(['Hamburguesa Morada', 'hamburguesasMorada.jpg','¿Una Burger vegetariana con sabor especial? Nueva Vegetal de color morado. 100% sabor a remolacha', 5.0]);
+hamburguesas.push(['Hamburguesa Roja', 'hamburguesasRoja.jpg','¿Una Burger vegetariana con sabor especial? Nueva Vegetal de color rosa. 100% sabor a fresa', 5.0]);
+hamburguesas.push(['Hamburguesa Verde', 'hamburguesasVerde.jpg','¿Una Burger vegetariana con sabor especial? Nueva Vegetal de color verde. 100% sabor a alga', 5.0]);
 
